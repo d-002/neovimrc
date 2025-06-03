2c48efeadbd4ccd0529ba046b01b439f261aa8f8
d-002 <minecraftenjoy@outlook.fr>
2025-06-03 20:40:52 +0200

    set up platform-dependent transparency

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,8 +1,10 @@
 local theme = require("onedark")
 
+local transparency = (vim.fn.has("wsl") or not vim.fn.has("windows")) ~= 0
+
 theme.setup {
     style = "warmer",
-    transparent = true,
+    transparent = transparency,
 
     lualine = {
         transparent = true,
